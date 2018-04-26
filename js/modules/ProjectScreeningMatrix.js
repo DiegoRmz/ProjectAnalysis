638dc705c8ca6a659b6fc495cad31d6fcf797f2d
Diego Ramírez <diegormz97@outlook.com>
2018-04-26 14:40:17 -0500

    Pushss

--- js/modules/ProjectScreeningMatrix.js
+++ js/modules/ProjectScreeningMatrix.js
@@ -10,24 +10,29 @@ factors.PRIMARY  = 0.4;
 factors.SECONDARY=0.35;
 factors.OTHER    =0.25;
 
+var history = {};
+
 //Trace to access elements, where it came from
 function calculateElement(idTrace,origin) {
-    console.log(idTrace);
-    //Alter place
-    var hold = parseFloat($("#"+idTrace+"_select").val())*parseFloat($("#"+idTrace+"_val").val());
+    _calculatePElement(idTrace,origin);
+}
 
-    subTotals[origin]-=prev;
-    subTotals[origin]+=hold;
+function _calculatePElement(idTrace, origin) {
+    var holder = parseFloat($("#"+idTrace+"_select").val())*parseFloat($("#"+idTrace+"_val").val());
 
-    prev = hold;
+    console.log(holder);
 
-    console.log(hold);
-    console.log(subTotals[origin]);
+    if(!history[idTrace])
+        history[idTrace] = 0;
+    
+    subTotals[origin] -= history[idTrace];
+    subTotals[origin] += holder;
 
-    //Alter fields
-    $("#"+origin+"_result").html(hold);
+    history[idTrace] = holder;
+
+    $("#"+idTrace+"_result").html(holder);
 
     $("#"+origin+"_TOTAL_W").html(subTotals[origin]);
 
-    $("#"+origin+"_cons").html(subTotals[origin]*factors[origin]);
+    $("#"+origin+"_cons").html(subTotals[origin]*factors[origin]);    
 }
